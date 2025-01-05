5fe6c014573d405f095d90dc3cbe2d30fe517330
KT-STL-Utsho <you@example.com>
2025-01-05 21:19:53 +0600

    final

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    methods: 'GET,POST,PUT,DELETE',
+    methods: 'GET,POST,PUT,DELETE, OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
   await app.listen(process.env.PORT ?? 3000);
